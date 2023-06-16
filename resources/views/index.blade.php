6135eabf46323dd1646e7cf2f15d94cecf4e0d5d
amine <bouramtaneamine1@gmail.com>
2023-06-16 23:53:03 +0100

    remove icon connect

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,11 +33,7 @@
             <form action="{{route("register")}}" method="POST">
                 @csrf
                 <h1>Create Account</h1>
-                <div class="social-container">
-                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-                </div>
+
                 <span>or use your email for registration</span>
                 <input type="text" placeholder="Name" name="name"/>
                 <input type="email" placeholder="Email" name="email"/>
