11bf9a39d9a341d6aa0949009ee44f18209cd7d6
amine <bouramtaneamine1@gmail.com>
2023-06-16 23:54:47 +0100

    remove icon of sign in

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,11 +45,7 @@
             <form action="{{route("login")}}" method="POST">
                 @csrf
                 <h1>Sign in</h1>
-                <div class="social-container">
-                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-                </div>
+
                 <span>or use your account</span>
                 <input type="email" placeholder="Email" name="email"/>
 
